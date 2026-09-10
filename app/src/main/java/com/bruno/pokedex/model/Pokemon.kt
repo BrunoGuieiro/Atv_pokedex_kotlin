4c50210152570d71a9932832ba264ad81472bbe3
BRUNO_GUIEIRO_LESSA <12402508@SIGMAB11.BARROCA>
2026-09-10 10:52:48 -0300

    atv feita

---
+++ app/src/main/java/com/bruno/pokedex/model/Pokemon.kt
@@ -0,0 +1,22 @@
+package com.bruno.pokedex.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonDetailResponse(
+    val id: Int,
+    val name: String,
+    val sprites: PokemonSprites
+)
+
+data class PokemonSprites(
+    @SerializedName("front_default") val frontDefault: String?,
+    val other: PokemonOtherSprites?
+)
+
+data class PokemonOtherSprites(
+    val showdown: PokemonShowdownSprites?
+)
+
+data class PokemonShowdownSprites(
+    @SerializedName("front_default") val frontDefault: String?
+)
